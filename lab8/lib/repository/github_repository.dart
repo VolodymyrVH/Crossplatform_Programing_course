00d83fefb4ad8e937a1ab22e490c97fa2a61bba7
VolodymyrVH <gorelovv515@gmail.com>
2025-11-07 20:41:29 +0100

    added lab8

---
+++ lab8/lib/repository/github_repository.dart
@@ -0,0 +1,13 @@
+import 'package:lab8/models/github_user.dart';
+import 'package:lab8/services/github_service.dart';
+
+class GitHubRepository {
+  final GitHubService _service;
+
+  GitHubRepository(this._service);
+
+  Future<GitHubUser> getUser(String username) async {
+    final data = await _service.fetchUserData(username);
+    return GitHubUser.fromJson(data);
+  }
+}
---
+++ lab8/lib/viewmodels/theme_view_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeViewModel extends ChangeNotifier {
+  static const String _themePreferenceKey = 'isDarkTheme';
+  bool _isDarkTheme = false;
+
+  bool get isDarkTheme => _isDarkTheme;
+
+  ThemeViewModel() {
+    _loadThemePreference();
+  }
+
+  void toggleTheme() {
+    _isDarkTheme = !_isDarkTheme;
+    _saveThemePreference();
+    notifyListeners();
+  }
+
+  Future<void> _loadThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkTheme = prefs.getBool(_themePreferenceKey) ?? false;
+    notifyListeners();
+  }
+
+  Future<void> _saveThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themePreferenceKey, _isDarkTheme);
+  }
+}

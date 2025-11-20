f62040df853f65d365b8f898dc5f659a3bda03d9
VolodymyrVH <gorelovv515@gmail.com>
2025-11-20 20:55:46 +0100

    added unit test

--- lab8/lib/viewmodels/theme_view_model.dart
+++ lab8/lib/viewmodels/theme_view_model.dart
@@ -7,21 +7,20 @@ class ThemeViewModel extends ChangeNotifier {
 
   bool get isDarkTheme => _isDarkTheme;
 
-  ThemeViewModel() {
-    _loadThemePreference();
-  }
+  ThemeViewModel();
 
-  void toggleTheme() {
-    _isDarkTheme = !_isDarkTheme;
-    _saveThemePreference();
-    notifyListeners();
-  }
-
-  Future<void> _loadThemePreference() async {
+  Future<void> init() async {
     final prefs = await SharedPreferences.getInstance();
     _isDarkTheme = prefs.getBool(_themePreferenceKey) ?? false;
     notifyListeners();
   }
+
+  Future<void> toggleTheme() async {
+  _isDarkTheme = !_isDarkTheme;
+  await _saveThemePreference();
+  notifyListeners();
+}
+
 
   Future<void> _saveThemePreference() async {
     final prefs = await SharedPreferences.getInstance();
